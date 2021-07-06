c4fec4d63b31a7c7c3cadc77b2138730fb131898
YouDontKnowThis <fox.physics@outlook.fr>
2021-07-06 05:21:29 +0100

    mode game setup

---
+++ src/script.ts
@@ -0,0 +1,52 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+// Canvas
+const canvas: HTMLCanvasElement = document.querySelector(".root");
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000);
+camera.position.z = 3;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+// controls.target.y = 2
+controls.enableDamping = true;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Animate
+const tick = () => {
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
