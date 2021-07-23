556e47fcd10c88817a5801a348600618fbfec163
clicoder <youhouma@studtent.1337.ma>
2021-07-23 16:34:20 +0100

    editing engine.js and adding serverNetworkManager

--- core/engine.js
+++ core/engine.js
@@ -1,12 +1,13 @@
 // Engine
 import * as BABYLON from "@babylonjs/core";
-import PlayerManager from "./playerManager.js";
 import "@babylonjs/core/Meshes/Builders/boxBuilder";
 
 // Files
 import "./utils/state.min.js";
 import generateWorld from "./world.js";
 import UnitManager from "./unitManager.js";
+import PlayerManager from "./playerManager.js";
+import ServerNetworkManager from "./networking/serverNetworkManager.js";
 
 // we can't use engine as keyword class because is reserved for this.noa Engine
 export class Engine {
@@ -14,54 +15,24 @@ export class Engine {
     this.noa = noa;
     this.unitManager = new UnitManager(this.noa);
     this.playerManager = new PlayerManager(this.noa);
-    this.body;
-    this.snapshot = [{ id: 1, position: [] }];
-    this.rotation = 0;
+    !global.isServer
+      ? (this.serverNetworkManager = new ServerNetworkManager(noa))
+      : "";
   }
   start() {
-    console.log("isServer", global.isServer);
     console.log("starting the this.noa engine...");
-    console.log("noa lookup", this.noa);
 
     generateWorld(this.noa);
     const scene = this.noa.rendering.getScene();
-    // Enable physics in the scene
     scene.enablePhysics(
       new BABYLON.Vector3(0, -9.8, 0),
       new BABYLON.AmmoJSPlugin()
     );
     this.body = this.playerManager.createPlayer(1);
-
     this.noa.on("tick", () => this.engineStep.bind(this)());
   }
   engineStep() {
-    let playerPositionChecker = [...this.noa.entities.getPosition(1)];
-    let current = this.noa.camera.getDirection()[0];
-    let persistanceRot = 0.01;
-    if (current > 0 && this.rotation !== current) {
-      this.body.rotatePOV(0, persistanceRot + 0.01, 0);
-    }
-    if (current < 0 && this.rotation !== current) {
-      this.body.rotatePOV(0, -persistanceRot - 0.01, 0);
-    }
-    this.rotation = current;
-    for (let elem in this.snapshot) {
-      // Get the position of each entity
-      this.noa.entities.getPosition(this.snapshot[elem].id);
-      if (this.snapshot[elem].id === 1) {
-        // if the id is the player entity id
-        let playerPosition = this.noa.entities.getPosition(1);
-        for (let elem in playerPosition) {
-          // check if the player has moved
-          if (playerPositionChecker[elem] !== playerPosition[elem]) {
-            playerPositionChecker = [...playerPosition];
-            // update the snapshot with new player position
-            this.snapshot[0].position = [...playerPosition];
-            console.log("the player of the entity with id === 1 has moved");
-          }
-        }
-      }
-    }
+    this.serverNetworkManager.createSnapshot(this.body);
   }
 }
 
---
+++ core/networking/serverNetworkManager.js
@@ -0,0 +1,45 @@
+class ServerNetworkManager {
+  constructor(noa) {
+    this.noa = noa;
+    this.snapshot = [{ id: 1, position: [] }];
+    this.rotation = 0;
+  }
+
+  // queueData() {}
+
+  //createSnapshot() {}
+
+  createSnapshot(body) {
+    // rotation will be moved in playerManager (Just to keep track of the code logic)
+    let playerPositionChecker = [...this.noa.entities.getPosition(1)];
+    let current = this.noa.camera.getDirection()[0];
+    let persistanceRot = 0.01;
+    if (current > 0 && this.rotation !== current) {
+      body.rotatePOV(0, persistanceRot + 0.01, 0);
+    }
+    if (current < 0 && this.rotation !== current) {
+      body.rotatePOV(0, -persistanceRot - 0.01, 0);
+    }
+    this.rotation = current;
+    // rotation will be moved in playerManager
+    for (let elem in this.snapshot) {
+      // Get the position of each entity
+      this.noa.entities.getPosition(this.snapshot[elem].id);
+      if (this.snapshot[elem].id === 1) {
+        // if the id is the player entity id
+        let playerPosition = this.noa.entities.getPosition(1);
+        for (let elem in playerPosition) {
+          // check if the player has moved
+          if (playerPositionChecker[elem] !== playerPosition[elem]) {
+            playerPositionChecker = [...playerPosition];
+            // update the snapshot with new player position
+            this.snapshot[0].position = [...playerPosition];
+            console.log("the player of the entity with id === 1 has moved");
+          }
+        }
+      }
+    }
+  }
+}
+
+export default ServerNetworkManager;
