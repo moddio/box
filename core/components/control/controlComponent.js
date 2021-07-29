f42d6ac6786433dd6875bb865c397e627c3395c6
clicoder <youhouma@studtent.1337.ma>
2021-07-29 18:15:43 +0100

    adding basic correction to keypress movement

--- core/components/control/controlComponent.js
+++ core/components/control/controlComponent.js
@@ -7,8 +7,12 @@ class ControlComponent {
   constructor(player) {
     this.player = player;
     //this.mainUnit = this.player.getMainUnit();
+    var lastUpdate = new Date().getTime();
     window.addEventListener("keypress", (e) => {
-      this.keyPress(e.key);
+      if (new Date().getTime() > lastUpdate + 100) {
+        this.keyPress(e.key);
+        lastUpdate = new Date().getTime();
+      }
     });
   }
 
@@ -18,24 +22,25 @@ class ControlComponent {
 
   keyPress(key) {
     const body = box.noa.entities.getPhysicsBody(this.player);
+
     // testing the control of the player
     // TODOO : TO STREAM KEY INPUT TO THE SERVER
     switch (key) {
       case "w":
         console.log("kepress", "w");
-        body.applyImpulse([0, 0, 5]);
+        body.applyImpulse([0, 0, 2.5]);
         break;
       case "s":
         console.log("kepress", "s");
-        body.applyImpulse([0, 0, -5]);
+        body.applyImpulse([0, 0, -2.5]);
         break;
       case "d":
         console.log("kepress", "d");
-        body.applyImpulse([5, 0, 0]);
+        body.applyImpulse([2.5, 0, 0]);
         break;
       case "a":
         console.log("kepress", "a");
-        body.applyImpulse([-5, 0, 0]);
+        body.applyImpulse([-2.5, 0, 0]);
         break;
       case "h":
         console.log("kepress", "h");
