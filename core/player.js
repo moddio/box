5969af2b7ccccb772b058b5fc92c019c32cc7d37
Jaeyun Noh <jaeyun@gmail.com>
2021-08-02 02:29:46 -0700

    adf

--- core/player.js
+++ core/player.js
@@ -3,11 +3,11 @@ import { Entity } from "./entity";
 export class Player extends Entity {
   constructor(data) {
     super();
-
+    this.name = data.name;
     //console.log("global", global.ControlComponent);
     if (box.isServer) {
       // add other player controls
-    } else (box.isClient && data.isMyPlayer == this){
+    } else (box.isClient){
       //console.log("control tewdwedewdew", controlComponent);
       //this.control = new controlComponent.ControlComponent(id);
       this.addComponent("ControlComponent");
