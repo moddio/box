13fdfab57c0cc77409d17a156c8425e7a08a4bda
clicoder <youhouma@studtent.1337.ma>
2021-08-06 16:41:05 +0100

    refactoring back the client network(30%)

--- server/server.js
+++ server/server.js
@@ -9,36 +9,12 @@ const io = new Server(server, {
   },
 });
 
-// removed blocks position [x,y,z]
-const removedBlocks = [];
-
-// socket logic
-const {
-  playersSocket,
-} = require("../core/components/network/serverNetworkEvent");
-const {
-  createBlockSocket,
-  removeBlockSocket,
-  returnCurrentBlocks,
-} = require("../core/components/network/clientNetworkEvent");
+var players = [];
 
 io.on("connection", (socket) => {
-  // Emit map state to new logged in user
-  socket.emit("mapBlockState", removedBlocks);
-  // Emit creation of block data to new user
-  socket.emit("createBlock", returnCurrentBlocks());
-  // Listen for position change or new player added
-  socket.on("players", ({ ID: playerID, position }) => {
-    playersSocket(playerID, position, socket);
-  });
-  // Listen for blocks creation
-  socket.on("createBlock", ({ data }) => {
-    createBlockSocket(data, socket);
-  });
-  // Listen for blocks removal
-  socket.on("removeBlock", ({ data: { position } }) => {
-    removedBlocks.push(position);
-    removeBlockSocket(position, socket);
+  socket.on("new-player", (data) => {
+    players.push(data);
+    console.log(players);
   });
 });
 
--- src/client.js
+++ src/client.js
@@ -1,3 +1,5 @@
+import { io } from "socket.io-client";
+
 let mapData = [
   [1, 2, 3, 4, 5, 6, 7, 8, 9],
   [1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -21,6 +23,15 @@ Box.loadScript(scriptData);
 // box.onEvent("playerJoin", function (player) {
 
 console.log("player has joined the game");
+
+// Client side server logic for socket
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+  console.log("my socket id is", socket.id);
+});
+
+socket.emit("new-player", { playerID: Math.random() });
 
 // when player joins the game, create a unit, and assign that unit to that player.
 // box.onEvent("playerJoin", function (player) {
