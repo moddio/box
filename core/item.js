b3d2eca2e1f12554b48436dabf6fdd88d0ba1cb9
clicoder <youhouma@studtent.1337.ma>
2021-09-02 16:31:01 +0100

    quick fix for item gun

--- core/item.js
+++ core/item.js
@@ -1,8 +1,8 @@
-import { Entity } from "./entity";
+import { Entity } from './entity';
 
 export class Item extends Entity {
   constructor(data) {
-    data.type = "item";
+    data.type = 'item';
     super(data);
 
     this.attachedTo = null;
@@ -12,32 +12,29 @@ export class Item extends Entity {
 
     this.width = data.width * this.radius;
     this.height = data.height * this.radius;
-    
+
     if (data.streamMode == undefined) {
-        this.streamMode = {
-            enabled: true,
-            stateChange: true,
-            attributes: false,
-            movement: false
-        };
+      this.streamMode = {
+        enabled: true,
+        stateChange: true,
+        attributes: false,
+        movement: false
+      };
     }
-    
-    
   }
 
   tick() {
     super.tick();
-    
+
     if (this.attachedTo) {
       console.log('attached');
-      
+
       //var playPos = BOX.Engine.noa.entities.getPosition(1);
       //var pos = [playPos[0], playPos[1] + 0.5, playPos[2] + 2];
       //this.body.setPosition([playPos[0], playPos[1] + 0.5, playPos[2] + 2]);
 
-      this.mesh.rotation = this.attachedTo.mesh.rotation;
+      this.attachedTo.mesh.rotation = this.mesh.rotation;
+    } else {
     }
-
   }
-
 }
